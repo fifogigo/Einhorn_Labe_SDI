04d0a1a3002b6e959c901bd1b44785152afd75b5
Labe Einhorn <fifogigo@gmail.com>
2015-05-12 04:41:54 -0500

    More variable work

---
+++ Variables_Defining/scripts/scripts.js
@@ -0,0 +1,35 @@
+/**
+ * Created by fifogigo on 5/12/2015.
+ */
+
+/*var age; // declaration
+age = 39; // definition
+//storage container = value
+
+var yearOfBirth = 1975; // declaring and defining on same line
+
+console.log(age);
+console.log(yearOfBirth);
+
+
+var isStudent = true;
+
+console.log(isStudent);
+
+var firstName = "Labe";
+
+console.log(firstName);
+
+var phrase = 'I don\'t know \nyou\'ll have to speak with the professor';
+
+console.log(phrase);*/
+
+var muppetNames = ["Kermit", "Miss Piggy"]; // 0, 1, 2, 3 , "Gonzo", "Rizzo"
+
+console.log(muppetNames);
+console.log(muppetNames[1]);
+
+muppetNames[1] = "Rizzo";
+muppetNames[2] = "Fozzy";
+console.log(muppetNames[1]);
+console.log(muppetNames[2]);
