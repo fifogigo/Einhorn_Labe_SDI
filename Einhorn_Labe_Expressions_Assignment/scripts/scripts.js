3433af7b6ab62135972e5f85a979a9da48c8380e
Labe Einhorn <fifogigo@gmail.com>
2015-05-13 02:06:36 -0500

    Changed alert to console log and put some test run output at the bottom of the JS file

--- Einhorn_Labe_Expressions_Assignment/scripts/scripts.js
+++ Einhorn_Labe_Expressions_Assignment/scripts/scripts.js
@@ -1,5 +1,6 @@
 /**
- * Created by fifogigo on 5/12/2015.
+ * Created by Labe Einhorn on 5/13/2015.
+ * Expression Assignment.
  */
 
 
@@ -17,16 +18,41 @@ vectorAdd1 =  [array1[0]+array2[0], array1[1]+array2[1]]; // calculate vector ad
 var arrayOutput1 = ("Adding (" + array1[0] + "," + array1[1] + ") + (" + array2[0] + "," + array2[1] + ")"); // this is the first part of the output
 var arrayOutput2 = ("\nGives you (" + vectorAdd1[0] + "," + vectorAdd1[1] + ")"); // second line of the output here
 
-alert(arrayOutput1 + arrayOutput2); // combine both lines of the output into an alert
+console.log(arrayOutput1 + arrayOutput2); // combine both lines of the output into an alert
 
-/*
-vectorAdd1 =  [Number(array1[0])+Number(array2[0]), Number(array1[1])+Number(array2[1])]; // calculate vector addition here
+// This would be more efficient in a loop, but haven't gotten that far in this class (though i'm pretty sure it's not much different from other languages
+array1[0]++; // incrementing value in array1[0]
+array1[1]++; // incrementing value in array1[1]
+array2[0]++; // incrementing value in array2[0]
+array2[1]++; // incrementing value in array2[1]
+
+vectorAdd1 =  [array1[0]+array2[0], array1[1]+array2[1]]; // calculate vector addition here
 var arrayOutput1 = ("Adding (" + array1[0] + "," + array1[1] + ") + (" + array2[0] + "," + array2[1] + ")"); // this is the first part of the output
 var arrayOutput2 = ("\nGives you (" + vectorAdd1[0] + "," + vectorAdd1[1] + ")"); // second line of the output here
 
-alert(arrayOutput1 + arrayOutput2); // combine both lines of the output into an alert
+console.log(arrayOutput1 + arrayOutput2); // combine both lines of the output into an alert
 
 
-console.log(vectorAdd1);
-console.log(array1);
-*/
+/*
+Test output run one:
+ Adding (8,13) + (26,7)
+ Gives you (34,20)
+
+ Adding (9,14) + (27,8)
+ Gives you (36,22)
+
+Test output run two:
+ Adding (9,45) + (65,12)
+ Gives you (74,57)
+
+ Adding (10,46) + (66,13)
+ Gives you (76,59)
+
+Test output run three:
+ Adding (98,2) + (78,23)
+ Gives you (176,25)
+
+ Adding (99,3) + (79,24)
+ Gives you (178,27)
+
+ */
