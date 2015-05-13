0feebf41b33f521017b45c05633a1202559333ee
Labe Einhorn <fifogigo@gmail.com>
2015-05-13 01:13:41 -0500

    Initial check in for assignment

---
+++ Einhorn_Labe_Expressions_Assignment/scripts/scripts.js
@@ -0,0 +1,13 @@
+/**
+ * Created by fifogigo on 5/12/2015.
+ */
+
+
+// First idea, lets see if we can get all the points while calculating a vector.....something
+// Ok, going to add a vector, increment the vector, and readd the vector
+var arrayElement1 = prompt("Please enter the X value for the first vector:"); // this is to get the first array element
+var arrayElement2 = prompt("Please enter the Y value for the first vector:"); // this is to get the second array element
+var arrayElement3 = prompt("Please enter the X value for the second vector:"); // this is to get the third array element
+var arrayElement4 = prompt("Please enter the Y value for the second vector:"); // this is to get the fourth array element
+
+console.log(arrayElement1 + "," +arrayElement2 + "," +arrayElement3 + "," + arrayElement4);
