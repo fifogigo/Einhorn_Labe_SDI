918bc4be16172fd882c2319d6e863317cc101810
Labe Einhorn <fifogigo@gmail.com>
2015-05-19 19:36:49 -0500

    Continuing basic skeleton

--- Einhorn_Labe_Conditionals_Assignment/scripts/scripts.js
+++ Einhorn_Labe_Conditionals_Assignment/scripts/scripts.js
@@ -2,4 +2,27 @@
  * Created by fifogigo on 5/12/2015.
  */
 
+// Working Idea, make a calculator that gets input, from user about watching a movie at a theater
+var isChild;
+var parentPresent;
+var movieRating;
+var movieName;
 
+prompt;// prompt to get isChild
+if(){
+    //check if variable assigned is a child and do if true
+}else if(){
+    //check variable if assigned if not a child
+}else{
+    //no other condition is true, reprompt for informtion
+}
+
+prompt; //prompt to see if parent is present
+prompt; //prompt to get movie rating
+if(){
+   // check isChild, parentPresent, and movieRating is true
+} else if(){
+    // check isChild, parentPresnt, and movieRating is false
+} else {
+    // reprompt for input
+}
