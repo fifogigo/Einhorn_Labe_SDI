79d4073b9dd3413013e5c66d5081bed01dc5ea6d
Labe Einhorn <fifogigo@gmail.com>
2015-05-19 22:45:42 -0500

    Basic structure and validation are done

--- Einhorn_Labe_Conditionals_Assignment/scripts/scripts.js
+++ Einhorn_Labe_Conditionals_Assignment/scripts/scripts.js
@@ -12,22 +12,25 @@ var childTicketPrice = 10.50; // using local theaters price
 var adultTicketPrice = 14.00; // using local theaters price
 
 childTickets = Number(prompt("Please enter number of children's tickets")); // get input
-console.log(childTickets); // check for numeric value
-if(childTickets == ""){
+console.log("Number of child tickets: " + childTickets); // check for numeric value
+if(childTickets === ""){
     childTickets = Number(prompt("Invalid number, Please enter number of children's tickets")); // get input
 }
 
 adultTickets = Number(prompt("Please enter number of adult's tickets")); // get input
-console.log(adultTickets); // check for numeric value
-if(adultTickets == ""){
+console.log("number of adult tickets: " + adultTickets); // check for numeric value
+if(adultTickets === ""){
     adultTickets = Number(prompt("Invalid number, Please enter number of adult's tickets")); // get input
 }
 
 movieName = (childTickets === 0) ? "Mad Max: Fury Road" : "Avengers: Age of Ultron (2015)"; // Ternary operator ftw
 console.log(movieName); // Verification that Ternary was setup correctly
 
-if (childTickets === 0) {
-
-} else if(adultTickets >= 0) {
-
+if (childTickets === 0 && adultTickets === 0) {
+    alert("No tickets are being purchased");
+    console.log("No tickets are being purchased");
+} else {
+    price = adultTickets * adultTicketPrice + childTickets * childTicketPrice;
+    alert(childTickets + " Children and " + adultTickets + " Adults are watching " + movieName + " for a total cost of $" + price.toFixed(2));
+    console.log(childTickets + " Children and " + adultTickets + " Adults are watching " + movieName + " for a total cost of $" + price.toFixed(2));
 }
