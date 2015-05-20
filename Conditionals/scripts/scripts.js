f6c3f6c02425623af820937dfc3360cf53a6331f
Labe Einhorn <fifogigo@gmail.com>
2015-05-19 19:26:22 -0500

    Conditional Videos and initial setup of the Conditionals Assignment

---
+++ Conditionals/scripts/scripts.js
@@ -0,0 +1,26 @@
+/**
+ * Created by fifogigo on 5/12/2015.
+ */
+/*
+var kidHeight = 52;
+var minHeight = 48;
+var wParentHeight = 45;
+
+// if the child is old enough, print to the console "You can ride!"
+// if the kid is over 48 inches in height
+
+if(kidHeight > minHeight) {
+    //you can ride
+    console.log("You can ride the coaster!");
+} else if(kidHeight > wParentHeight) {
+    // you can ride with a parent present
+    console.log("You can ride, but only with a parent present.")
+} else {
+    //code performed if condition is false
+    console.log("Sorry kid, you've got some growing to do first!")
+}
+*/
+
+var gpa = 4.0;
+
+(gpa>2.0) ? console.log("You can graduate!") : console.log("GPA is too low!");
