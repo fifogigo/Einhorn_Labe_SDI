6285ef7af0ffa92345e8fb88fb2f0a86b99d2259
Labe Einhorn <fifogigo@gmail.com>
2015-05-19 23:03:55 -0500

    Added a third condition, validation, and logic to calculate end result

--- Einhorn_Labe_Conditionals_Assignment/scripts/scripts.js
+++ Einhorn_Labe_Conditionals_Assignment/scripts/scripts.js
@@ -10,6 +10,8 @@ var movieName; // variable for movie name
 var price = 0; // sum will be here
 var childTicketPrice = 10.50; // using local theaters price
 var adultTicketPrice = 14.00; // using local theaters price
+var childrenAsAdults;
+var numberOfChildrenAsAdults;
 
 childTickets = Number(prompt("Please enter number of children's tickets")); // get input
 console.log("Number of child tickets: " + childTickets); // check for numeric value
@@ -21,6 +23,22 @@ adultTickets = Number(prompt("Please enter number of adult's tickets")); // get 
 console.log("number of adult tickets: " + adultTickets); // check for numeric value
 if(adultTickets === ""){
     adultTickets = Number(prompt("Invalid number, Please enter number of adult's tickets")); // get input
+}
+
+childrenAsAdults = prompt("Are any children coming in as adults? (Y/N)"); // get input
+console.log("Children coming in as adults: " + childrenAsAdults); // check for numeric value
+if(childrenAsAdults === "Y" || childrenAsAdults === "y"){
+    numberOfChildrenAsAdults = Number(prompt("Please enter number of children coming in as adults")); // get input
+    if(numberOfChildrenAsAdults > childTickets){
+        numberOfChildrenAsAdults = Number(prompt("The number was invalid, please try again:")); // get input
+    } else {
+        childTickets -= numberOfChildrenAsAdults;
+        adultTickets += numberOfChildrenAsAdults;
+    }
+} else if(childrenAsAdults === "N" || childrenAsAdults === "n") {
+    console.log("No children are coming in as adults")
+} else {
+    childrenAsAdults = prompt("That was an invalid response, please try again");
 }
 
 movieName = (childTickets === 0) ? "Mad Max: Fury Road" : "Avengers: Age of Ultron (2015)"; // Ternary operator ftw
