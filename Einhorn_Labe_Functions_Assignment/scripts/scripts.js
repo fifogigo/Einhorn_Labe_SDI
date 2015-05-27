aa0a82666f520fd25f802d4dca0db6ecb0126ad1
Labe Einhorn <fifogigo@gmail.com>
2015-05-27 02:31:07 -0500

    Second function done, math-apocalypse averted

--- Einhorn_Labe_Functions_Assignment/scripts/scripts.js
+++ Einhorn_Labe_Functions_Assignment/scripts/scripts.js
@@ -15,8 +15,10 @@ function getUserInput(inputMessage){
 }
 
 // Function to calculate new minimum balance based off of current balance, amount paid, and apr
-var calculateBalance = function(){
-
+// not sure how to do this calculation so i'm guessing until I can find what the actual calculation is
+var calculateBalance = function(balance, payment, apr){
+    console.log("passed in values " + balance, payment, apr);
+    return balance - payment + (apr/1200) * balance;
 }
 
 // Function to display output
@@ -28,3 +30,9 @@ function displayOutput(){
 
 var currentBalance = Number(getUserInput("Please enter the current balance"));
 console.log("Returned value is: " + currentBalance);
+var paymentAmount = Number(getUserInput("Please enter the amount paid"));
+console.log("Returned value is: " + paymentAmount);
+var currentApr = Number(getUserInput("Please enter the current apr"));
+console.log("Returned value is: " + currentApr);
+var total = calculateBalance(currentBalance, paymentAmount, currentApr);
+console.log("Remaining amount " + total);
