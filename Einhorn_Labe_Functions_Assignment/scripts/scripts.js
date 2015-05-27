83ee3c15cf47a0da62d68eed9b6cde9654116a90
Labe Einhorn <fifogigo@gmail.com>
2015-05-27 01:32:55 -0500

    Commit for lessons and initial commit for homework assignment

---
+++ Einhorn_Labe_Functions_Assignment/scripts/scripts.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Labe Einhorn on 5/27/2015.
+ * Functions Assignment.
+ */
+
+// Function to get input
+
+
+// Function to calculate new minimum balance based off of current balance, amount paid, and apr
+
+
+// Function to display output
+
+
+// Main document here
+
---
+++ Functions/scripts/scripts.js
@@ -0,0 +1,22 @@
+/**
+ * Created by fifogigo on 5/12/2015.
+ */
+
+/*function outputMsg(){
+    console.log("Hello World");
+}
+
+function calcArea(w, h){
+    //var area = w * h;
+    return w * h;
+}
+
+var total = calcArea(30,20);
+console.log(total);*/
+
+var calcArea = function(width, height){
+    return width * height;
+}
+
+var a = calcArea(20,30);
+console.log(a);
